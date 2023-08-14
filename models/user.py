57a413ac477fe71e21e054aaa2c90f073ab4f7dd
HossamMahmoudKhedr <nhem318@gmail.com>
2023-08-14 12:27:48 +0200

    Modify User class

--- models/user.py
+++ models/user.py
@@ -5,10 +5,10 @@ from models.base_model import BaseModel
 
 class User(BaseModel):
     ''' Class for modeling a User entity '''
+    email = ''
+    password = ''
+    first_name = ''
+    last_name = ''
     def __init__(self, *args, **kwargs):
         ''' Initialiser of the State class '''
-        self.email = ''
-        self.password = ''
-        self.first_name = ''
-        self.last_name = ''
         super().__init__(**kwargs)
